fa98235507aca68ad065cd380d820f793eff9ba9
cdacamara <dacamara.cameron@gmail.com>
2016-08-02 10:22:02 -0500

    parser cleanup

--- parse/parser.cpp
+++ parse/parser.cpp
@@ -12,9 +12,15 @@
 
 namespace { // utils
 
-std::string make_error(const bf::lex::token& tok) {
+std::string make_error_unexpected(const bf::lex::token& tok) {
   std::stringstream ss;
   ss << tok.pos.name << '(' << tok.pos.line << ':' << tok.pos.col << ')' << ": unexpected token: " << static_cast<char>(tok.type);
+  return ss.str();
+}
+
+std::string make_error_custom(const bf::lex::token& tok, const std::string& msg) {
+  std::stringstream ss;
+  ss << tok.pos.name << '(' << tok.pos.line << ':' << tok.pos.col << ')' << ": " << msg << " token: " << static_cast<char>(tok.type);
   return ss.str();
 }
 
@@ -27,44 +33,61 @@ namespace parse {
 bool parser::expect_(lex::token_t tok) {
   if (c_tok_.type != tok) {
     using namespace std::literals;
-    auto str = make_error(c_tok_);
+    auto str = make_error_unexpected(c_tok_);
     str += " got: "s + static_cast<char>(c_tok_.type);
     return false;
   }
   next_token_();
   return true;
 }
+std::unique_ptr<cmd::command> parser::parse_stmt_() {
+  std::unique_ptr<cmd::command> stmt;
+
+  switch (c_tok_.type) {
+    case lex::token_t::l_shift:
+      stmt = std::make_unique<cmd::shift>(cmd::shift::shift_type::left);
+      break;
+    case lex::token_t::r_shift:
+      stmt = std::make_unique<cmd::shift>(cmd::shift::shift_type::right);
+      break;
+    case lex::token_t::inc:
+      stmt = std::make_unique<cmd::arithmetic>(cmd::arithmetic::arith_type::add);
+      break;
+    case lex::token_t::dec:
+      stmt = std::make_unique<cmd::arithmetic>(cmd::arithmetic::arith_type::sub);
+      break;
+    case lex::token_t::out:
+      stmt = std::make_unique<cmd::io>(cmd::io::io_type::out);
+      break;
+    case lex::token_t::in:
+      stmt = std::make_unique<cmd::io>(cmd::io::io_type::in);
+      break;
+    case lex::token_t::l_bracket:
+    case lex::token_t::r_bracket:
+    case lex::token_t::eof:
+      errors_.emplace_back(make_error_unexpected(c_tok_));
+      return nullptr; // we can't hit this while in a loop
+  }
+  next_token_(); // eat 'command'
+
+  return stmt;
+}
 
 std::unique_ptr<cmd::loop> parser::parse_loop_() {
   cmd::loop::cmd_list_t commands;
 
+  auto c = c_tok_;
   if (!expect_(lex::token_t::l_bracket)) return nullptr; // eat '['
 
   while (c_tok_.type != lex::token_t::r_bracket) {
     switch (c_tok_.type) {
       case lex::token_t::l_shift:
-        commands.emplace_back(std::make_unique<cmd::shift>(cmd::shift::shift_type::left));
-        next_token_();
-        break;
       case lex::token_t::r_shift:
-        commands.emplace_back(std::make_unique<cmd::shift>(cmd::shift::shift_type::right));
-        next_token_();
-        break;
       case lex::token_t::inc:
-        commands.emplace_back(std::make_unique<cmd::arithmetic>(cmd::arithmetic::arith_type::add));
-        next_token_();
-        break;
       case lex::token_t::dec:
-        commands.emplace_back(std::make_unique<cmd::arithmetic>(cmd::arithmetic::arith_type::sub));
-        next_token_();
-        break;
       case lex::token_t::out:
-        commands.emplace_back(std::make_unique<cmd::io>(cmd::io::io_type::out));
-        next_token_();
-        break;
       case lex::token_t::in:
-        commands.emplace_back(std::make_unique<cmd::io>(cmd::io::io_type::in));
-        next_token_();
+        commands.emplace_back(parse_stmt_());
         break;
       case lex::token_t::l_bracket:
         {
@@ -74,6 +97,7 @@ std::unique_ptr<cmd::loop> parser::parse_loop_() {
         }
         break;
       case lex::token_t::eof:
+        errors_.emplace_back(make_error_custom(c, "error trying to match"));
         errors_.emplace_back("unexpected EOF");
         return nullptr; // we can't hit this while in a loop
     }
@@ -95,28 +119,12 @@ prog::program parser::parse() {
   do {
     switch (c_tok_.type) {
       case lex::token_t::l_shift:
-        p.emplace_back(std::make_unique<cmd::shift>(cmd::shift::shift_type::left));
-        next_token_();
-        break;
       case lex::token_t::r_shift:
-        p.emplace_back(std::make_unique<cmd::shift>(cmd::shift::shift_type::right));
-        next_token_();
-        break;
       case lex::token_t::inc:
-        p.emplace_back(std::make_unique<cmd::arithmetic>(cmd::arithmetic::arith_type::add));
-        next_token_();
-        break;
       case lex::token_t::dec:
-        p.emplace_back(std::make_unique<cmd::arithmetic>(cmd::arithmetic::arith_type::sub));
-        next_token_();
-        break;
       case lex::token_t::out:
-        p.emplace_back(std::make_unique<cmd::io>(cmd::io::io_type::out));
-        next_token_();
-        break;
       case lex::token_t::in:
-        p.emplace_back(std::make_unique<cmd::io>(cmd::io::io_type::in));
-        next_token_();
+        p.emplace_back(parse_stmt_());
         break;
       case lex::token_t::l_bracket:
         {
@@ -131,7 +139,7 @@ prog::program parser::parse() {
         }
         break;
       case lex::token_t::r_bracket:
-        errors_.emplace_back(make_error(c_tok_));
+        errors_.emplace_back(make_error_unexpected(c_tok_));
         p.clear(); // ill-formed
         c_tok_.type = lex::token_t::eof;
         break;
